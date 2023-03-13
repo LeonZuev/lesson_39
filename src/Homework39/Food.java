04ceb7ecc6704e07bd7807ca28877c0b75dd92c3
Leon Zuev <loonatune@gmail.com>
2023-03-14 01:50:30 +0200

    Add Homework package. class Food, constructor, setters, getters

---
+++ src/Homework39/Food.java
@@ -0,0 +1,34 @@
+package Homework39;
+
+/*
+
++ поле "название"
++ поле "калорийность"
+конструктор, геттеры, сеттеры
+метод "употребить еду" с каким-нибудь выводом
+ */
+public class Food {
+
+  private String name;
+  private String kcal;
+
+  public Food(String name, String kcal) {
+    this.name = name;
+    this.kcal = kcal;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+  public void setKcal(String kcal) {
+    this.kcal = kcal;
+  }
+
+  public String getName() {
+    return name;
+  }
+  public  String getKcal() {
+    return kcal;
+  }
+
+}
