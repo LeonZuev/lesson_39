2f9a1bbfedcca6675eba79c144459a262652f40e
Leon Zuev <loonatune@gmail.com>
2023-03-14 16:55:03 +0200

    Add constructor, fix bugs in openString method

--- src/Homework39/Drink.java
+++ src/Homework39/Drink.java
@@ -4,9 +4,9 @@ package Homework39;
   В классе должны быть дополнительно:
 + поле "газированный" (да/нет)
 + поле "алкогольный" (да/нет)
-метод "открыть", который:
-для газированного напитка печатает "пшш",
-для негазированного - "скр",
+  метод "открыть", который:
++ для газированного напитка печатает "пшш",
++ для негазированного - "скр",
 а для алкогольного сначала спрашивает возраст и издаёт звук, только если возраст больше 18.
  */
 
@@ -14,24 +14,28 @@ public class Drink extends Food {
 
   private boolean soda;
   private boolean alco;
+  private boolean age;
+
+  public Drink(String name, int kcal) {
+    super(name, kcal);
+  }
+
 
   public String openString() {
-    if (soda = true) {
-      return "Пшш";
-    }
-    else {
-      return "скр";
-    }
-    if (alco = true) {
-      System.out.println("Исполнилось 18?");
-      private boolean age;
-      if (age = true) {
-        return "Ябадабадуу!!!";
-      }
-      else {
-        return "до 18-ти пъём только минералку!!!";
-      }
-    }
+   if (alco) {
+     if (age) {
+       return "Ябадабаду!!!";
+     }
+     else {
+       return "До 18-ти пъём только минералку!";
+     }
+   } else {
+       if (soda) {
+         return "Пшш";
+       } else {
+         return "Скр";
+       }
+     }
+   }
   }
-}
 
