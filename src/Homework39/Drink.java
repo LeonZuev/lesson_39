3c92cf7be56e924246761516ecd71f6cb9158030
Leon Zuev <loonatune@gmail.com>
2023-03-14 15:16:11 +0200

    Add openString method.

--- src/Homework39/Drink.java
+++ src/Homework39/Drink.java
@@ -15,7 +15,23 @@ public class Drink extends Food {
   private boolean soda;
   private boolean alco;
 
-  public String open
-
-
+  public String openString() {
+    if (soda = true) {
+      return "Пшш";
+    }
+    else {
+      return "скр";
+    }
+    if (alco = true) {
+      System.out.println("Исполнилось 18?");
+      private boolean age;
+      if (age = true) {
+        return "Ябадабадуу!!!";
+      }
+      else {
+        return "до 18-ти пъём только минералку!!!";
+      }
+    }
+  }
 }
+
